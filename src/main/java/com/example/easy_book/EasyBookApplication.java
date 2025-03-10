495b2c0e250e6823689d5b8fc12e3428573e1aca
Tsvetelin <enchev.tsvetelin@gmail.com>
2025-03-10 18:06:39 +0200

    Project initialization

---
+++ src/main/java/com/example/easy_book/EasyBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.easy_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBookApplication.class, args);
+	}
+
+}
